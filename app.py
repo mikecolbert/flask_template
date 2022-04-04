4bb7d55cf3ba27173cad32240ce2a3c1e56b1ded
Caleb Kitzman <caleb-kitzman@uiowa.edu>
2022-04-04 14:32:40 -0500

    vtm_site initial commit

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
+@app.route('/about')
+def about():
     return render_template('mike.html')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
